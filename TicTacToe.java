3d8e49a11de96639d1f36163d2a479ceb97a3867
hemalathareddy20 <hemalathareddy401@gmail.com>
2020-12-03 11:35:12 +0530

    Added:logic to choose input on gameboard

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,9 +1,11 @@
+import java.util.Scanner;
 public class TicTacToe {
 
    public static void main(String[] args){
           System.out.println("Welcome to TicTacToe program");
           char[] board = createBoard();
-   }
+          assignBoard();
+ }
 
    public static char[] createBoard() {
           char[] board=new char[10];
@@ -11,5 +13,17 @@ public class TicTacToe {
              board[i]= ' ';
    }
      return board;
+}
+   public static String assignBoard() {
+        String player;
+        String computer;
+      	Scanner s=new Scanner(System.in);
+        System.out.println("enter ur choice 0 or x");
+        player=s.next();
+        if(player=="0")
+             computer="0";
+        else
+             computer="x";
+           return player;
   }
 }
