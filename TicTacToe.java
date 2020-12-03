5bdbb46983a297170118bb98c81893ee69fe633e
hemalathareddy20 <hemalathareddy401@gmail.com>
2020-12-03 14:35:57 +0530

    Refactor:logic to move cell number in gameboard

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class TicTacToe {
    String player;
    String computer;
@@ -8,7 +9,8 @@ public class TicTacToe {
           TicTacToe Board= new TicTacToe();
           Board.assignBoard();
           showBoard(board);
-  }
+          int userMove = getUserMove(board, userInput);
+   }
 
    public static char[] createBoard() {
           char[] board=new char[10];
@@ -31,5 +33,17 @@ public class TicTacToe {
         for(int i=0;i<3;i++) {
             System.out.println(board[i]+"|"+board[i+1]+"|"+board[i+2]+"|");
         }
+   }
+   private static int getUserMove(char[] board,Scanner userInput) {
+           Integer [] validCells={1,2,3,4,5,6,7,8,9};
+           while(true){
+           System.out.println("enter your next move(1-9):");
+           int index = userInput.nextInt();
+           if (Arrays.asList(validCells).contains(index) && isSpaceFree(board,index))
+               return index;
+           }
+   }
+   private static boolean isSpaceFree(char[] board, int index) {
+     return board[index] == ' ';
    }
 }
