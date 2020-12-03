1b408a66826d6ee099b83b71d2f4600828e3871e
hemalathareddy20 <hemalathareddy401@gmail.com>
2020-12-03 11:00:59 +0530

    Added:logic to create gameboard

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,5 +1,15 @@
-public class TicTacToe{
-public static void main(String[] args){
-System.out.println("Welcome to TicTacToe program");
-}
+public class TicTacToe {
+
+   public static void main(String[] args){
+          System.out.println("Welcome to TicTacToe program");
+          char[] board = createBoard();
+   }
+
+   public static char[] createBoard() {
+          char[] board=new char[10];
+   for(int i=1;i<=10;i++){
+             board[i]= ' ';
+   }
+     return board;
+  }
 }
